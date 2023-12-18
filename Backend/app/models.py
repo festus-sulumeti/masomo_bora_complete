d0ae41a3d3c1bf489b3a5825cb7d4243bbbff626
festus-sulumeti <sulumetifestus@outlook.com>
2023-12-18 16:24:36 +0300

    THe modefied files

--- Backend/app/models.py
+++ Backend/app/models.py
@@ -6,6 +6,14 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+class Student(Base):
+    __tablename__ = "students"
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String, index=True)
+    last_name = Column(String, index=True)
+    email = Column(String, index=True, unique=True)
+    phone_number = Column(String)
+
 class Team(Base):
     __tablename__ = "teams"
     id = Column(Integer, primary_key=True, index=True)
