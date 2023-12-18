bf25ea6fbbfcc2818ee7299d925575abf3b6a977
festus-sulumeti <sulumetifestus@outlook.com>
2023-12-18 03:09:05 +0300

    THe app.js file

--- masomo_bora/src/App.js
+++ masomo_bora/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+// App.js
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Detail from './Components/Detail';
+// import Signup from './Components/Signup';
+import Signin from './Components/Signin';
+import Home from './Components/Home/Home';
+import ManageTeam from './Components/Home/ManageTeam';
+import ContactsInformation from './Components/Home/ContactsInformation';
+import InvoicesBalances from './Components/Home/InvoicesBalances';
+import StudentForm from './Components/Home/StudentForm';
+import General from './Components/Home/General';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Detail />} />
+          {/* <Route path="/Signup" element={<Signup />} /> */}
+          <Route path="/Signin" element={<Signin />} />
+          <Route path="/Home" element={<Home />}>
+            <Route path="General" element={<General />} />
+            <Route path="ManageTeam" element={<ManageTeam />} />
+            <Route path="ContactsInformation" element={<ContactsInformation />} />
+            <Route path="InvoicesBalances" element={<InvoicesBalances />} />
+            <Route path="StudentForm" element={<StudentForm />} />
+            
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
