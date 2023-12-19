f5efa6c548266db681afa00eef915a44b974a635
festus-sulumeti <sulumetifestus@outlook.com>
2023-12-19 18:27:39 +0300

    THe project files

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -2,9 +2,9 @@
 
 from fastapi import FastAPI, HTTPException, Depends
 from sqlalchemy.orm import Session
-from Backend.app import crud  # Adjust the import statement
+from Backend.app import crud
 from Backend.app.database import engine, SessionLocal
-from Backend.app import models  # Adjust the import statement
+from Backend.app import models
 
 app = FastAPI()
 
@@ -15,3 +15,40 @@ def get_db():
         yield db
     finally:
         db.close()
+
+# # Example route for the root
+# @app.get("/")
+# def read_root():
+#     return {"message": "Hello, Masomo Bora!"}
+
+
+# Route for ManageTeam
+@app.get("/Home/ManageTeam")
+def read_team_info(db: Session = Depends(get_db)):
+    # Fetch team information from the database
+    
+
+    # Example: Get a list of teams from the database
+    teams = crud.get_teams(db)
+
+    # Process the data as needed
+    team_data = [{"id": team.id, "name": team.name} for team in teams]
+
+    # Return the processed data
+    return {"team_data": team_data}
+
+@app.get("/Home/StudentForm")
+def read_student_form(db: Session = Depends(get_db)):
+    # Fetch student form information from the database
+    
+
+    # Example: Get a list of students from the database
+    students = crud.get_students(db)
+
+    # Process the data as needed
+    student_data = [{"id": student.id, "name": f"{student.first_name} {student.last_name}"} for student in students]
+
+    # Return the processed data
+    return {"student_data": student_data}
+
+
